e23ee2404101214ac6c2eb03485e76a644914f7c
Dydek123 <mazurkiewicz707@gmail.com>
2021-04-08 19:05:12 +0200

    Create mocks data to test program

---
+++ test/mocks/mockSinglePost.ts
@@ -0,0 +1,10 @@
+import postI from "../../src/interfaces/postI";
+
+const mockSinglePost: postI = {
+    userId: 1,
+    id: 4,
+    title: "Lorem ipsum",
+    body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+}
+
+export default mockSinglePost
